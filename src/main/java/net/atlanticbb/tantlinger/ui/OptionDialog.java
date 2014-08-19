8720263be19c33e76cbc111c3c22f41c324402ed
Andreas Rudolph <andy@openindex.de>
2014-08-19 02:58:25 +0200

    Added possibility to hide the "hyperlink" image in the hyperlink dialog for better integrability.
    merged from https://github.com/VEDAGroup/SHEF/commit/052cc642fb392822816f2dddfb510451824c0a2b?w=1

--- src/main/java/net/atlanticbb/tantlinger/ui/OptionDialog.java
+++ src/main/java/net/atlanticbb/tantlinger/ui/OptionDialog.java
@@ -11,18 +11,18 @@ import javax.swing.*;
 
 
 public class OptionDialog extends StandardDialog
-{    
+{
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
     private JPanel internalContentPane;
     private Container contentPane;
-    
+
     public OptionDialog(Frame parent, String headerTitle, String desc, Icon icon)
     {
         super(parent, headerTitle, BUTTONS_RIGHT);
-        init(headerTitle, desc, icon);        
+        init(headerTitle, desc, icon);
     }
 
     public OptionDialog(Dialog parent, String headerTitle, String desc, Icon icon)
@@ -30,30 +30,32 @@ public class OptionDialog extends StandardDialog
         super(parent, headerTitle, BUTTONS_RIGHT);
         init(headerTitle, desc, icon);
     }
-    
+
     private void init(String title, String desc, Icon icon)
     {
         internalContentPane = new JPanel(new BorderLayout());
-        HeaderPanel hp = new HeaderPanel();
-        hp.setTitle(title);
-        hp.setDescription(desc);
-        hp.setIcon(icon);
-        //hp.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
-        internalContentPane.add(hp, BorderLayout.NORTH);
-        
+        if(desc != null || icon != null)
+        {
+            HeaderPanel hp = new HeaderPanel();
+            hp.setTitle(title);
+            hp.setDescription(desc);
+            hp.setIcon(icon);
+            //hp.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+            internalContentPane.add(hp, BorderLayout.NORTH);
+        }
         super.setContentPane(internalContentPane);
     }
-    
+
     public Container getContentPane()
     {
         return contentPane;
     }
-    
+
     public void setContentPane(Container c)
     {
         //internalContentPane.remove(contentPane);
         contentPane = c;
         internalContentPane.add(c, BorderLayout.CENTER);
-        
+
     }
 }
--- src/main/java/net/atlanticbb/tantlinger/ui/text/dialogs/HyperlinkDialog.java
+++ src/main/java/net/atlanticbb/tantlinger/ui/text/dialogs/HyperlinkDialog.java
@@ -20,26 +20,27 @@ import net.atlanticbb.tantlinger.ui.UIUtils;
 public class HyperlinkDialog extends HTMLOptionDialog
 {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
 
     private static final I18n i18n = I18n.getInstance("net.atlanticbb.tantlinger.ui.text.dialogs");
-    
+
     private static Icon icon = UIUtils.getIcon(UIUtils.X48, "link.png"); //$NON-NLS-1$
     private static String title = i18n.str("hyperlink"); //$NON-NLS-1$
     private static String desc = i18n.str("hyperlink_desc"); //$NON-NLS-1$
-    
+
+    private static boolean showHeader = true;
     private LinkPanel linkPanel;
-    
+
     public HyperlinkDialog(Frame parent)
     {
-        this(parent, title, desc, icon, true);
+        this(parent, title, showHeader?desc:null, showHeader?icon:null, true);
     }
-    
+
     public HyperlinkDialog(Dialog parent)
     {
-        this(parent, title, desc, icon, true);        
+        this(parent, title, showHeader?desc:null, showHeader?icon:null, true);
     }
 
     public HyperlinkDialog(Dialog parent, String title, String desc, Icon ico, boolean urlFieldEnabled)
@@ -52,39 +53,46 @@ public class HyperlinkDialog extends HTMLOptionDialog
     {
         super(parent, title, desc, ico);
         init(urlFieldEnabled);
-    }    
-    
+    }
+
     private void init(boolean urlFieldEnabled)
     {
         linkPanel = new LinkPanel(urlFieldEnabled);
         linkPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setContentPane(linkPanel);
-        setSize(315, 370);
+        if(showHeader)
+        {
+            setSize(315, 370);
+        }
+        else
+        {
+            setSize(315, 314);
+        }
         setResizable(false);
     }
-    
+
     public Map getAttributes()
     {
         return linkPanel.getAttributes();
     }
-    
+
     public void setAttributes(Map attribs)
     {
         linkPanel.setAttributes(attribs);
     }
-    
+
     public void setLinkText(String text)
     {
         linkPanel.setLinkText(text);
     }
-    
+
     public String getLinkText()
     {
         return linkPanel.getLinkText();
     }
 
     public String getHTML()
-    {        
+    {
         String html = "<a"; //$NON-NLS-1$
         Map ht = getAttributes();
         for(Iterator e = ht.keySet().iterator(); e.hasNext();)
@@ -92,11 +100,15 @@ public class HyperlinkDialog extends HTMLOptionDialog
             Object k = e.next();
             html += " " + k + "=" + "\"" + ht.get(k) + "\""; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         }
-        
+
         html += ">" + getLinkText() + "</a>"; //$NON-NLS-1$ //$NON-NLS-2$
-               
-        
+
+
         return html;
     }
 
+    public static void setHeaderVisible(boolean showHeader)
+    {
+        HyperlinkDialog.showHeader = showHeader;
+    }
 }
